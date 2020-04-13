574cac47f7df4f96349d700589b95900e884a11b
randypx <sinrandy@hotmail.com>
2020-04-13 12:09:47 +0800

    created radial menu

---
+++ Origami-Unity/Assets/Scripts/ScrollGesture.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Microsoft.MixedReality.Toolkit.Input;
+
+public class ScrollGesture : MonoBehaviour, IMixedRealityGestureHandler<Vector3>
+{
+    public float rotationSpeed = 1;
+
+    public void OnGestureStarted(InputEventData eventData)
+    {
+        string inputActionType = eventData.MixedRealityInputAction.Description;
+        Debug.Log($"{inputActionType} Gesture started for {gameObject.name}");
+
+        if (inputActionType == "Navigation Action")
+        {
+            gameObject.transform.localScale = new Vector3(1.05f, 1.05f, 1f);
+        }
+    }
+
+    public void OnGestureUpdated(InputEventData eventData)
+    {
+        string inputActionType = eventData.MixedRealityInputAction.Description;
+        Debug.Log($"{inputActionType} Gesture updated for {gameObject.name}. No Action Taken");
+    }
+
+    public void OnGestureUpdated(InputEventData<Vector3> eventData)
+    {
+        string inputActionType = eventData.MixedRealityInputAction.Description;
+        Debug.Log($"{inputActionType} Gesture updated for {gameObject.name}: {eventData.InputData}");
+
+        if (inputActionType == "Navigation Action")
+        {
+            Vector3 inputDir = gameObject.transform.rotation * eventData.InputData;
+            gameObject.transform.Rotate(new Vector3(0f, 0f, (inputDir.x - inputDir.z) * rotationSpeed));
+            Debug.Log($"Rotate on Z axis by {inputDir.x - inputDir.z}");
+        }
+    }
+
+    public void OnGestureCompleted(InputEventData eventData)
+    {
+        string inputActionType = eventData.MixedRealityInputAction.Description;
+        Debug.Log($"{inputActionType} Gesture completed for {gameObject.name}. No action taken");
+    }
+
+    public void OnGestureCompleted(InputEventData<Vector3> eventData)
+    {
+        string inputActionType = eventData.MixedRealityInputAction.Description;
+        Debug.Log($"{inputActionType} Gesture completed for {gameObject.name}");
+
+        if (inputActionType == "Navigation Action")
+        {
+            gameObject.transform.localScale = new Vector3(1f, 1f, 1f);
+        }
+    }
+
+    public void OnGestureCanceled(InputEventData eventData)
+    {
+        string inputActionType = eventData.MixedRealityInputAction.Description;
+        Debug.Log($"{inputActionType} Gesture cancelled for {gameObject.name}");
+
+        if (inputActionType == "Navigation Action")
+        {
+            gameObject.transform.localScale = new Vector3(1f, 1f, 1f);
+        }
+    }
+}
---
+++ Origami-Unity/Assets/Scripts/StepMenuController.cs
@@ -0,0 +1,112 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+namespace Microsoft.MixedReality.Toolkit.UI
+{
+    public class StepMenuController : MonoBehaviour
+    {
+        public GameObject stepsMenu;
+        public GameObject stepButtonPrefab;
+        public GameObject stepMenuCircle;
+        public UIController uiController;
+        public Theme buttonTheme;
+        public States buttonsStates;
+
+        private readonly float menuRadius = -13.2f;
+        private readonly float angle = 7f;
+
+        private void Awake()
+        {
+            //For testing
+            populateStepsMenu(1, 10);
+        }
+
+        public void displayStepSelectionScreen(string origami)
+        {
+            Debug.Log("Switching to Steps Menu");
+            int stepsTotal = getStepTotal(origami);
+            int currentStep = uiController.getCurrentStep();
+            populateStepsMenu(currentStep, stepsTotal);
+            stepsMenu.SetActive(true);
+        }
+
+        public void selectStep(int stepNo)
+        {
+            Debug.Log($"Step {stepNo} selected");
+            //unpopulateMenu();
+            //stepsMenu.SetActive(false);
+            //uiController.displayInstructionMenu(stepNo);
+        }
+
+        private void RotateMenu(float deg)
+        {
+            stepMenuCircle.transform.Rotate(0, 0, -deg);
+        }
+
+        private int getStepTotal(string origami)
+        {
+            //Return a fixed number for now
+            return 16;
+        }
+
+        private void populateStepsMenu(int current, int total)
+        {
+            for (int x = 0; x < total; x++)
+            {
+                //Position button accordingly
+                Vector3 pos = new Vector3(Mathf.Cos(x * angle * Mathf.Deg2Rad) * menuRadius, Mathf.Sin(x * angle * Mathf.Deg2Rad) * menuRadius, 0);
+                Quaternion rot = Quaternion.Euler(0f, 0f, x * angle);
+                GameObject buttonInstance = Instantiate(stepButtonPrefab, stepMenuCircle.transform.position + pos, rot, stepMenuCircle.transform);
+
+                //Change number accordingly
+                GameObject grandchild = buttonInstance.transform.GetChild(0).GetChild(0).gameObject;
+                if (grandchild.name == "Number")
+                {
+                    TMP_Text stepNo = grandchild.GetComponent<TMP_Text>();
+                    stepNo.text = (x + 1).ToString();
+                }
+                else
+                {
+                    Debug.LogError("Fail to find Number grandchild of Button Prefab");
+                }
+
+                //Add interable component
+                Interactable interactable = buttonInstance.AddComponent<Interactable>();
+                interactable.States = buttonsStates;
+                interactable.Profiles = new List<InteractableProfileItem>()
+                {
+                    new InteractableProfileItem()
+                    {
+                        Themes = new List<Theme>() { buttonTheme },
+                        Target = buttonInstance
+                    }
+                };
+                Debug.Log($"Button No {x + 1}");
+                interactable.OnClick.AddListener(() => this.selectStep((x + 1)));
+
+                //Disable the currently chosen step
+                if (x == current - 1)
+                {
+                    interactable.IsEnabled = false;
+                }
+
+            }
+            RotateMenu((current - 1) * angle);
+        }
+
+        private void unpopulateMenu()
+        {
+            int childCount = stepMenuCircle.transform.childCount;
+            for (int x = 0; x < childCount; x++)
+            {
+                if (stepMenuCircle.transform.GetChild(x).name.Contains("Button"))
+                {
+                    Destroy(stepMenuCircle.transform.GetChild(x).gameObject);
+                }
+            }
+        }
+    }
+}
